1e3bbb18183da878576e4af72a2a13c557fd07f5
karina <karina@karinas-Air.lan>
2025-01-07 18:53:25 +0200

    first commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const randomNumberFeedback = document.querySelector('#randomNumber');
 const generateRandomBtn = document.querySelector('#generateRandomBtn');
 
+
+
+
 generateRandomBtn.addEventListener('click', () => {
     //const randomNumber = Math.floor(Math.random() * (11 - 1) + 1); //1 - 10 -  Math.random() * (max - min) + min;
     const randomNumber = Math.floor(Math.random() * 10) + 1;
